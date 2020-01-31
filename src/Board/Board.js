e59b06dcf5d49507ac9dc04bf58a0c6c0a31e5c6
Lara Balsamello <lara.balsamello@gmail.com>
2020-01-31 14:45:05 -0300

    Add: robot defends board logic

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -7,105 +7,243 @@ class Board extends Component {
 
     state = {
         fullBoard: [null, null, null, null, null, null, null, null, null],
-        playerActive: Math.round(Math.random())
+        playerActive: Math.round(Math.random()),
+        enemyLastsPositions: []
     };
 
     clickHandler = (id) => {
         let newState = { ...this.state };
         newState.fullBoard[id] = this.state.playerActive;
         let newFullboard = newState.fullBoard;
-        this.setState({ fullBoard: newFullboard });
+        let positions = [...this.state.enemyLastsPositions];
+        positions.push(id);
+        this.setState({ fullBoard: newFullboard, enemyLastsPositions: positions });
         this.togglePlayer();
     }
 
-    hasWon() {
+    hasWon = () => {
         console.log('who won?')
         let boardForWin = this.state.fullBoard;
-        if(boardForWin[0]===0&&boardForWin[1]===0&&boardForWin[2]===0) {
+        if (boardForWin[0] === 0 && boardForWin[1] === 0 && boardForWin[2] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[3]===0&&boardForWin[4]===0&&boardForWin[5]===0) {
+        } else if (boardForWin[3] === 0 && boardForWin[4] === 0 && boardForWin[5] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[7]===0&&boardForWin[8]===0&&boardForWin[9]===0) {
+        } else if (boardForWin[7] === 0 && boardForWin[8] === 0 && boardForWin[9] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[0]===1&&boardForWin[1]===1&&boardForWin[2]===1) {
+        } else if (boardForWin[0] === 1 && boardForWin[1] === 1 && boardForWin[2] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[3]===1&&boardForWin[4]===1&&boardForWin[5]===1) {
+        } else if (boardForWin[3] === 1 && boardForWin[4] === 1 && boardForWin[5] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[7]===1&&boardForWin[8]===1&&boardForWin[9]===1) {
+        } else if (boardForWin[7] === 1 && boardForWin[8] === 1 && boardForWin[9] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[0]===0&&boardForWin[3]===0&&boardForWin[6]===0) {
+        } else if (boardForWin[0] === 0 && boardForWin[3] === 0 && boardForWin[6] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[1]===0&&boardForWin[4]===0&&boardForWin[7]===0) {
+        } else if (boardForWin[1] === 0 && boardForWin[4] === 0 && boardForWin[7] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[2]===0&&boardForWin[5]===0&&boardForWin[8]===0) {
+        } else if (boardForWin[2] === 0 && boardForWin[5] === 0 && boardForWin[8] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[0]===1&&boardForWin[3]===1&&boardForWin[6]===1) {
+        } else if (boardForWin[0] === 1 && boardForWin[3] === 1 && boardForWin[6] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[1]===1&&boardForWin[4]===1&&boardForWin[7]===1) {
+        } else if (boardForWin[1] === 1 && boardForWin[4] === 1 && boardForWin[7] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[2]===1&&boardForWin[5]===1&&boardForWin[8]===1) {
+        } else if (boardForWin[2] === 1 && boardForWin[5] === 1 && boardForWin[8] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[0]===0&&boardForWin[4]===0&&boardForWin[8]===0) {
+        } else if (boardForWin[0] === 0 && boardForWin[4] === 0 && boardForWin[8] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[2]===0&&boardForWin[4]===0&&boardForWin[7]===0) {
+        } else if (boardForWin[2] === 0 && boardForWin[4] === 0 && boardForWin[7] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[2]===0&&boardForWin[5]===0&&boardForWin[8]===0) {
+        } else if (boardForWin[2] === 0 && boardForWin[5] === 0 && boardForWin[8] === 0) {
             console.log(0 + "HAS WON")
-        } else if(boardForWin[0]===1&&boardForWin[3]===1&&boardForWin[6]===1) {
+        } else if (boardForWin[0] === 1 && boardForWin[3] === 1 && boardForWin[6] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[1]===1&&boardForWin[4]===1&&boardForWin[7]===1) {
+        } else if (boardForWin[1] === 1 && boardForWin[4] === 1 && boardForWin[7] === 1) {
             console.log(1 + "HAS WON")
-        } else if(boardForWin[2]===1&&boardForWin[5]===1&&boardForWin[8]===1) {
+        } else if (boardForWin[2] === 1 && boardForWin[5] === 1 && boardForWin[8] === 1) {
             console.log(1 + "HAS WON")
-        } 
+        } else if (boardForWin[0] === 1 && boardForWin[4] === 1 && boardForWin[8] === 1) {
+            console.log(1 + "HAS WON")
+        } else if (boardForWin[2] === 1 && boardForWin[4] === 1 && boardForWin[6] === 1) {
+            console.log(1 + "HAS WON")
+        } else if (boardForWin[0] === 0 && boardForWin[4] === 0 && boardForWin[8] === 0) {
+            console.log(0 + "HAS WON")
+        } else if (boardForWin[2] === 0 && boardForWin[4] === 0 && boardForWin[6] === 0) {
+            console.log(0 + "HAS WON")
+        }
     }
 
     componentDidMount() {
         console.log(this.state.playerActive, "playa");
-        if(this.state.playerActive===0) {
+        if (this.state.playerActive === 0) {
             this.robotMoves();
-        } 
-    }
-
-    componentDidUpdate(){
-        console.log('update')
-        if(this.state.playerActive===0) {
-            this.robotMoves();
-        } else {
-            console.log('adjkasjd');
         }
     }
 
-    robotMoves() {
-        console.log('robot moving');
-        let indexesAvaible = [];
-        this.state.fullBoard.map((box, i) => {
-            if(box===null){
-                indexesAvaible.push(i)
-            }
-            return indexesAvaible;
-        });
-        let position = Math.floor((Math.random() * (indexesAvaible.length-1)));
-        let newBoard = [...this.state.fullBoard];
-        newBoard[indexesAvaible[position]] = 0;
-        this.setState({fullBoard: newBoard, playerActive: 1});
+    componentDidUpdate() {
+        console.log('update')
+        if (this.state.playerActive === 0) {
+            this.robotMoves();
+        } else {
+            console.log('player turn');
+        }
     }
+
+    robotMoves = () => {
+        //This function is for the movility of the robot represented by the circles.
+        //The function below grabs all the available positions on the current board.
+
+        this.analizeBoard()
+
+    }
+    moveRandomly = (indexesAvailable) => {
+        let position = Math.floor((Math.random() * (indexesAvailable.length - 1)));
+        let newBoard = [...this.state.fullBoard];
+        newBoard[indexesAvailable[position]] = 0;
+        this.setState({ fullBoard: newBoard, playerActive: 1 });
+        console.log('ofense')
+    }
+
+    ///_______________________________________________________________________
+
+    analizeBoard() {
+        let indexesAvailable = [];
+        this.state.fullBoard.map((box, i) => {
+            if (box === null) {
+                indexesAvailable.push(i)
+            }
+            return indexesAvailable;
+        });
+        let enemy = this.state.enemyLastsPositions;
+        if (enemy.length <= 1) {
+            this.moveRandomly(indexesAvailable);
+        } else {
+            //bloque para movimientos en horizontal 
+
+            if (enemy[0] === 0 && enemy[1] === 1) {
+                this.moveRobotSetNewBoard(2, indexesAvailable);
+            } else if (enemy[0] === 1 && enemy[1] === 0) {
+                this.moveRobotSetNewBoard(2, indexesAvailable);
+            } else if (enemy[0] === 1 && enemy[1] === 2) {
+                this.moveRobotSetNewBoard(0, indexesAvailable);
+            } else if (enemy[0] === 2 && enemy[1] === 1) {
+                this.moveRobotSetNewBoard(0, indexesAvailable);
+            } else if (enemy[0] === 3 && enemy[1] === 4) {
+                this.moveRobotSetNewBoard(5, indexesAvailable);
+            } else if (enemy[0] === 4 && enemy[1] === 3) {
+                this.moveRobotSetNewBoard(5, indexesAvailable);
+            } else if (enemy[0] === 4 && enemy[1] === 5) {
+                this.moveRobotSetNewBoard(3, indexesAvailable)
+            } else if (enemy[0] === 5 && enemy[1] === 4) {
+                this.moveRobotSetNewBoard(3, indexesAvailable);
+            } else if (enemy[0] === 6 && enemy[1] === 7) {
+                this.moveRobotSetNewBoard(8, indexesAvailable);
+            } else if (enemy[0] === 7 && enemy[1] === 6) {
+                this.moveRobotSetNewBoard(8, indexesAvailable);
+            } else if (enemy[0] === 7 && enemy[1] === 8) {
+                this.moveRobotSetNewBoard(6, indexesAvailable)
+            } else if (enemy[0] === 8 && enemy[1] === 7) {
+                this.moveRobotSetNewBoard(6, indexesAvailable);
+
+                //bloque para movimientos en vertical 
+
+            } else if (enemy[0] + 3 === enemy[1] && enemy[1] + 3 <= 8) {
+                let moveTo = enemy[1] + 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] + 3 === enemy[0] && enemy[0] + 3 <= 8) {
+                let moveTo = enemy[0] + 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[0] - 3 === enemy[1] && enemy[1] - 3 >= 0) {
+                let moveTo = enemy[1] - 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] - 3 === enemy[0] && enemy[0] - 3 >= 0) {
+                let moveTo = enemy[0] - 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            }
+            //bloques para salteo horizontal
+
+            else if (enemy[0] + 2 === enemy[1] && enemy[0] + 1 <= 8 && enemy[0] + 1 <= 8) {
+                let moveTo = enemy[0] + 1;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] + 2 === enemy[0] && enemy[1] + 1 <= 8) {
+                let moveTo = enemy[1] + 1;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[0] - 2 === enemy[1] && enemy[1] + 1 >= 0) {
+                let moveTo = enemy[1] + 1;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] - 2 === enemy[0] && enemy[0] + 1 >= 0) {
+                let moveTo = enemy[0] + 1;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            }
+
+            //bloques para salteo vertical
+
+            else if (enemy[0] + 6 === enemy[1] && enemy[0] + 3 <= 8) {
+                let moveTo = enemy[0] + 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] + 6 === enemy[0] && enemy[1] + 3 <= 8) {
+                let moveTo = enemy[1] + 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[0] - 6 === enemy[1] && enemy[1] - 3 >= 0) {
+                let moveTo = enemy[1] - 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            } else if (enemy[1] - 6 === enemy[0] && enemy[0] - 3 >= 0) {
+                let moveTo = enemy[0] - 3;
+                this.moveRobotSetNewBoard(moveTo, indexesAvailable);
+            }
+
+            //bloque para diagonal
+
+            else if (enemy[0] === 0 && enemy[1] === 4) {
+                this.moveRobotSetNewBoard(8, indexesAvailable);
+            } else if (enemy[0] === 4 && enemy[1] === 0) {
+                this.moveRobotSetNewBoard(8, indexesAvailable);
+            } else if (enemy[0] === 4 && enemy[1] === 8) {
+                this.moveRobotSetNewBoard(0, indexesAvailable);
+            } else if (enemy[0] === 8 && enemy[1] === 4) {
+                this.moveRobotSetNewBoard(0, indexesAvailable);
+            } else if (enemy[0] === 6 && enemy[1] === 2) {
+                this.moveRobotSetNewBoard(4, indexesAvailable)
+            } else if (enemy[1] === 6 || enemy[0] === 2) {
+                this.moveRobotSetNewBoard(4, indexesAvailable)
+            } else if (enemy[0] === 0 && enemy[1] === 8) {
+                this.moveRobotSetNewBoard(4, indexesAvailable)
+            } else if (enemy[1] === 0 || enemy[0] === 8) {
+                this.moveRobotSetNewBoard(4, indexesAvailable)
+            } else {
+                this.moveRandomly();
+            }
+        }
+
+    }
+
+    moveRobotSetNewBoard = (index, indexesAvaible) => {
+        indexesAvaible.map(available => {
+            if (available === index) {
+                let newBoard = [...this.state.fullBoard];
+                newBoard[index] = 0;
+                this.setState({ fullBoard: newBoard, playerActive: 1 });
+            }
+            return console.log('moving robot')
+        });
+    }
+
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::..::::::::::::::::::::::::::
 
     setInitialPlayer = () => {
         this.setState({ playerActive: Math.round(Math.random()) })
     }
 
-    togglePlayer  = () => {
+    togglePlayer = () => {
         let player = this.state.playerActive;
         if (player === 1) {
-            this.setState({playerActive: 0})
-        } else if(player === 0) {
-            this.setState({playerActive: 1})
-        }     
-        this.hasWon();
-    } 
+            this.setState({ playerActive: 0 })
+        } else if (player === 0) {
+            this.setState({ playerActive: 1 })
+        }
+    }
 
     render() {
+        this.hasWon();
         let board = [];
         for (let i = 0; i < this.state.fullBoard.length; i++) {
             board.push(
